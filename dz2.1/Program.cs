292a8f2f63a0e38f820a3030e9dd10181a7736bd
shiirik <mr.pakman04@gmail.com>
2023-10-26 09:28:41 +0600

    Добавьте файлы проекта.

---
+++ dz2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст, чтобы подсчитать количество пробелов. Введите точку (.) для завершения.");
+
+        int spaceCount = 0;
+        char input;
+
+        do
+        {
+            input = Console.ReadKey().KeyChar;
+            if (input == ' ')
+            {
+                spaceCount++;
+            }
+        } while (input != '.');
+
+        Console.WriteLine($"\nКоличество пробелов: {spaceCount}");
+    }
+}
